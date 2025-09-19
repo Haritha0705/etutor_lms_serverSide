d54ad6c97347bd7a13cc4116f894b8b40d1679ee
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-09-19 14:20:39 +0530

    create end point create in SubCategory

---
+++ src/api/courses/subCategory.service.ts
@@ -0,0 +1,43 @@
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '../../config/prisma/prisma.service';
+import { CreateSubCategoryDto } from './dto/create-subCategory.dto';
+
+@Injectable()
+export class SubCategoryService {
+  private readonly logger = new Logger(SubCategoryService.name);
+  constructor(private readonly DB: PrismaService) {}
+
+  /** Create a new sub Category */
+  async createSubCategory(createSubCategoryDto: CreateSubCategoryDto) {
+    try {
+      const category = await this.DB.category.findUnique({
+        where: { id: createSubCategoryDto.categoryId },
+      });
+
+      if (!category) {
+        throw new NotFoundException(
+          `SubCategory with id ${createSubCategoryDto.categoryId} not found`,
+        );
+      }
+
+      const subCategory = await this.DB.subCategory.create({
+        data: {
+          name: createSubCategoryDto.name,
+          count: createSubCategoryDto.count,
+          categoryId: createSubCategoryDto.categoryId,
+        },
+      });
+
+      this.logger.log(`Tool created with ID ${subCategory.id}`);
+      return subCategory;
+    } catch (error) {
+      this.logger.error(`Failed to create tool: ${error.message}`, error.stack);
+      throw new BadRequestException('Unable to create tool');
+    }
+  }
+}
